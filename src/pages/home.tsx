a61ab2a2b323f827c4a73c5cdaff94e726fbced5
VihoSerge <komi.vihoserge@gmail.com>
2023-05-05 19:29:44 +0000

    ...

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,7 +3,7 @@ import Logo from "@/components/ui/logo";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center min-h-screen gap-8 p-8 pt-32">
+    <div className="flex flex-col items-center min-h-screen gap-8 p-8 lg:pt-32">
       <Logo />
       <div className="md:w-2/3 text-center space-y-3">
         <p className="text-4xl">Welcome to NASA Media Library!</p>
